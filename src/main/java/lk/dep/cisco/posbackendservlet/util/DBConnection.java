8275372ea66cf645842220285fd50a3bb9c030fa
cisco <sovisrushain@gmail.com>
2021-10-02 10:22:19 +0530

    Add DBConnection

---
+++ src/main/java/lk/dep/cisco/posbackendservlet/util/DBConnection.java
@@ -0,0 +1,21 @@
+package lk.dep.cisco.posbackendservlet.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/dep7_backup_pos", "root", "cisco12345Z$");
+    }
+
+}
