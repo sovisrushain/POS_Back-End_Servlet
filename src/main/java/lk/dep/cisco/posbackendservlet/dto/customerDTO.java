ca589649425b9a98e5a0f11cf76d2bbe99f22f6e
cisco <sovisrushain@gmail.com>
2021-10-02 10:28:55 +0530

    Add customerDTO and ItemDTO

---
+++ src/main/java/lk/dep/cisco/posbackendservlet/dto/customerDTO.java
@@ -0,0 +1,49 @@
+package lk.dep.cisco.posbackendservlet.dto;
+
+public class customerDTO {
+    private String id;
+    private String name;
+    private String address;
+
+    public customerDTO() {
+    }
+
+    public customerDTO(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "customerDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
