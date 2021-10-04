853cce0b2bb51865d2f3cdb60173125f91568f27
cisco <sovisrushain@gmail.com>
2021-10-04 22:22:41 +0530

    Add doGet and doPost methods in customerServlet

--- src/main/java/lk/dep/cisco/posbackendservlet/api/CustomerServlet.java
+++ src/main/java/lk/dep/cisco/posbackendservlet/api/CustomerServlet.java
@@ -1,29 +1,76 @@
 package lk.dep.cisco.posbackendservlet.api;
 
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import lk.dep.cisco.posbackendservlet.dto.CustomerDTO;
+import lk.dep.cisco.posbackendservlet.exception.DuplicateIdentifierException;
+import lk.dep.cisco.posbackendservlet.exception.FailedOperationException;
+import lk.dep.cisco.posbackendservlet.exception.NotFoundException;
+import lk.dep.cisco.posbackendservlet.service.CustomerService;
+import lk.dep.cisco.posbackendservlet.util.DBConnection;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "CustomerServlet", value = "/customers")
 public class CustomerServlet extends HttpServlet {
 
+    private final Jsonb jsonb = JsonbBuilder.create();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         /* 1. Connect to the DB */
-        /* 2. Fetch Customers */
-        /* 3. Convert to Json */
-        /* 4. Send back to client */
+        try(Connection connection = DBConnection.getConnection()){
+            /* 2. Fetch Customers */
+            CustomerService customerService = new CustomerService(connection);
+            String id = req.getParameter("id");
+            List<CustomerDTO> customers = new ArrayList<>();
+            if(id == null){
+                customers = customerService.findAllCustomer();
+            }else {
+                customers.add(customerService.findCustomer(id));
+            }
+            /* 3. Convert to Json */
+            String json = jsonb.toJson(id == null ? customers : customers.get(0));
+            /* 4. Send back to client */
+            resp.setContentType("application/json");
+            PrintWriter out = resp.getWriter();
+            out.println(json);
+        } catch (SQLException | FailedOperationException | NotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         /* 1. Determine weather the content type is Json */
+        if(!req.getContentType().equals("application/json") || req.getContentType() == null){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
         /* 2. Connect to the DB */
-        /* 3. Bind Json to Java Obj */
-        /* 4. Save the customer */
-        /* 5. Send a response to the client */
+        try(Connection connection = DBConnection.getConnection()){
+            /* 3. Bind Json to Java Obj */
+            CustomerDTO customer = jsonb.fromJson(req.getReader(), CustomerDTO.class);
+            /* 4. Save the customer */
+            /* ToDo: Validation */
+            CustomerService customerService = new CustomerService(connection);
+            customerService.saveCustomer(customer);
+            /* 5. Send a response to the client */
+            resp.setContentType("application/json");
+            PrintWriter out = resp.getWriter();
+            out.println(jsonb.toJson(customer.getId()));
+        } catch (SQLException | DuplicateIdentifierException | FailedOperationException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
